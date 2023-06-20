7ea7c52abb4cce7db1da5bcf35208fa72349727e
T1Erick <guevaraerick49@gmail.com>
2023-06-20 18:23:22 -0500

    Servicios y entidades Erick guevara

--- src/app/entities/user.model.ts
+++ src/app/entities/user.model.ts
@@ -11,10 +11,10 @@ export interface UsersModel{
 }
 
 export interface CreateUserModelDto extends Omit<UsersModel,'id' | 'rol'>{
-  rolid:number
+  rolId:number
 }
 
 export interface UpdateUserModelDto extends Partial<UsersModel>{
   id:string;
-  rolid?:number
+  rolId?:number
 }
