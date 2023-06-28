3be09c551ffdf2676d5a4a1e303ab5e7a1c0391b
T1Erick <guevaraerick49@gmail.com>
2023-06-28 17:51:48 -0500

    Cambios y imprfesion Erick

--- src/app/pages/bill/bill.component.ts
+++ src/app/pages/bill/bill.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import  jsPDF from 'jspdf';
+import { ProductsModel } from 'src/app/entities/products.model';
 
 @Component({
   selector: 'app-bill',
   templateUrl: './bill.component.html',
   styleUrls: ['./bill.component.css']
 })
-export class BillComponent {
+export class BillComponent implements OnInit {
+
+  ngOnInit() {
+    this.imprimirButton = document.querySelector('button') as HTMLButtonElement;
+    this.imprimirButton.addEventListener('click', this.imprimirFactura);
+    this.products = history.state
+  }
+
+
+
+  products:ProductsModel[]=[]
+
+
+
+  imprimirButton: HTMLButtonElement | null;
+
+  constructor() {
+    this.imprimirButton = null;
+  }
+
+
+
+  imprimirFactura() {
+    window.print();
+  }
+
 
 
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CreateProductsModelDto, ProductsModel } from 'src/app/entities/products.model';
+import { CategoryModel } from 'src/app/entities/category.model';
+import {  ProductsModel } from 'src/app/entities/products.model';
+import { SectionModel } from 'src/app/entities/section.model';
 import { CategorService } from 'src/app/services/category.service.service';
 import { ProductsService } from 'src/app/services/products.service';
+import { SectionService } from 'src/app/services/section.service.service';
 
 @Component({
   selector: 'app-products',
@@ -10,14 +13,19 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit  {
-  constructor(private productService: ProductsService, private router: Router,){}
+  constructor(private productService: ProductsService, private router: Router, private sectionService:SectionService, private registerCategory: CategorService,){}
   ngOnInit():void{
-    this.getProducts()
+    this.getProducts();
+    this.getSeccion();
+    this.getCategories();
 
   }
   searchTerm: string = '';
-  listaProductos: any[] = [];
-  products: ProductsModel[]=[]
+  seccion: SectionModel[]=[]
+  productosSeleccionados: ProductsModel[] = [];
+  products: ProductsModel[]=[];
+  category: CategoryModel[]=[]
+
   getProducts(){
     this.productService.getAll().subscribe(
 
@@ -27,6 +35,35 @@ export class ProductsComponent implements OnInit  {
 
       }
     )
+  }
+  getSeccion(){
+    this.sectionService.getAll().subscribe(
+      response =>{
+        console.log(this.seccion);
+        this.seccion =response
+
+      }
+    )
+  }
+  getCategories(){
+    this.registerCategory.getAll().subscribe(
+
+      response =>{
+        this.category = response;
+        console.log(this.category)
+
+      }
+    )
+
+  }
+  agregarProduct(product: ProductsModel){
+    this.productosSeleccionados.push(product);
+    console.log('Producto agregado:', product);
+    console.log('Listado', this.productosSeleccionados)
+
+  }
+  NavigateToBill(){
+    this.router.navigateByUrl("dashboard/bill");
   }
 
 
