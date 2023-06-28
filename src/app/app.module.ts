946a2205b21f1d6b49a86c6b21ae366f7805102c
T1Erick <guevaraerick49@gmail.com>
2023-06-28 07:19:25 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,9 @@ import { PagesRoutingModule } from './pages/pages-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
-<<<<<<< HEAD
+import { ProductnamePipe } from './pipe/productname.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-=======
-import { ProductnamePipe } from './pipe/productname.pipe';
->>>>>>> 310211bfaa0e72e1e7c5b217c0b55ade63c8e8c0
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
