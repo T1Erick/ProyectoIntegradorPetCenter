eda1e1e2056d97e8e067c063b4d29c13e96e26b1
Jessica-Gaby <jgm.ayala@yavirac.edu.ec>
2023-07-03 09:43:42 -0500

    perfil

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardClientComponent } from './dashboard-client/dashboard-client.component';
-import { ForumPetComponent } from './forum-pet/forum-pet.component';
 import { ProductsComponent } from './products/products.component';
 import { RouterModule } from '@angular/router';
 import { SocialNetworkComponent } from './social-network/social-network.component';
